48aa346108d5a8fb72a7ab4d0c20b5b1fe4b5b83
Nichlas Torgersen <nichlas@coretrek.no>
2023-09-15 20:58:01 +0200

    changes to increments

--- src/App/App.vue
+++ src/App/App.vue
@@ -3,9 +3,16 @@
         <TransitionGroup name="fade" mode="out-in">
             <template v-for="(string, index) in stringsByCategory">
                 <div v-if="index === currentIndex" class="flex flex-col text-white text-6xl absolute whitespace-pre-line" :key="index">
-                    <span class="block text-stone-500 text-lg">
+                    <span class="flex text-stone-500 text-sm items-center justify-end fill-stone-500 mb-7">
+                        <button class="p-2" @click.prevent="updateStringIndex({incrementBy: -1})">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32C28.7 32 0 60.7 0 96L0 416zM128 256c0-6.7 2.8-13 7.7-17.6l112-104c7-6.5 17.2-8.2 25.9-4.4s14.4 12.5 14.4 22l0 208c0 9.5-5.7 18.2-14.4 22s-18.9 2.1-25.9-4.4l-112-104c-4.9-4.5-7.7-10.9-7.7-17.6z"/></svg>
+                        </button>
                         ({{ index + 1 }} / {{ stringsByCategory.length }})
+                        <button class="p-2" @click.prevent="updateStringIndex()">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M448 96c0-35.3-28.7-64-64-64L64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320zM320 256c0 6.7-2.8 13-7.7 17.6l-112 104c-7 6.5-17.2 8.2-25.9 4.4s-14.4-12.5-14.4-22l0-208c0-9.5 5.7-18.2 14.4-22s18.9-2.1 25.9 4.4l112 104c4.9 4.5 7.7 10.9 7.7 17.6z"/></svg>
+                        </button>
                     </span>
+
                     {{ string }}
                 </div>
             </template>
